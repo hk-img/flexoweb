4e9f2a5706687b8262e2329c3de09ebccfb29b41
Bharat Sharma <bharatsharma@gmail.com>
2024-11-20 16:24:44 +0530

    change in env

--- generate-env.js
+++ generate-env.js
@@ -4,16 +4,19 @@ const dotenv = require('dotenv');
 // Load environment variables from the .env file
 const env = dotenv.config().parsed;
 
-// Define the `environment` object
+// Log the env object to check if `WEB_DOMAIN` is correctly loaded
+console.log(env);
+
+// Define the `environment` object with the new webDomain variable
 const environmentContent = `
 export const environment = {
   production: ${env.PRODUCTION === 'true'},
   apiUrl: "${env.API_URL}",
   s3_bucket_path: "${env.S3_BUCKET_PATH}",
-  mapKey: "${env.MAP_KEY}"
+  mapKey: "${env.MAP_KEY}",
+  webDomain: "${env.WEB_DOMAIN || ''}"
 };
 `;
 
 // Write the generated environment.ts file
 fs.writeFileSync('./src/environments/environment.ts', environmentContent.trim());
-
