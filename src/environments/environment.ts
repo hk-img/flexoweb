ce39e0e6bc6c4b3d7c1cb2a58ebef2459da2193c
hk-img <34542482+hk-img@users.noreply.github.com>
2025-01-30 23:57:51 +0530

    fixed error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
   s3_bucket_path: "worker-app",
   mapKey: "AIzaSyCOAhd885zH5TvinKCm3-rhDvgndqzoQyU",
   webDomain: "",
-  clientId: "696066348928-dpugnnhq6v9ou191b40dcm7mmh4esuto.apps.googleusercontent.com"
+  clientId: "696066348928-dpugnnhq6v9ou191b40dcm7mmh4esuto.apps.googleusercontent.com",
+  HOST_WEBLINK: "googleusercontent.com"
 };
