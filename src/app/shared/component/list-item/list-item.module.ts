89a0f0246ab31f9fddc3dc4adee063ef77f6cff3
Girish-120 <98391965+Girish-120@users.noreply.github.com>
2025-05-29 13:24:46 +0530

    Update list-item.module.ts

--- src/app/shared/component/list-item/list-item.module.ts
+++ src/app/shared/component/list-item/list-item.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ListItemComponent } from './list-item.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, SlickCarouselModule, RouterModule],
+  imports: [CommonModule, SlickCarouselModule, RouterModule,SharedModule],
   declarations: [ListItemComponent],
   exports: [ListItemComponent],
 })
