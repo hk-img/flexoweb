0dc12eb29bbb80b445194150b685ae6975446be4
hk-img <34542482+hk-img@users.noreply.github.com>
2025-06-12 09:42:26 +0530

    chore: include SharedModule where app-icon is used

--- src/app/booking-list/booking-list.module.ts
+++ src/app/booking-list/booking-list.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
 import { BookingListComponent } from './booking-list.component';
+import { SharedModule } from '../shared/shared/shared.module';
 
 const routes: Routes = [{ path: '', component: BookingListComponent }];
 
 @NgModule({
   declarations: [BookingListComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule, SharedModule, RouterModule.forChild(routes)],
   providers: [DatePipe]
 })
 export class BookingListModule {}
