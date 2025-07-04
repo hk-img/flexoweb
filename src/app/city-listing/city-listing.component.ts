ebb7c2cf6e1ef1cd94b8b2255d4e4a9b4327b382
Girish-120 <98391965+Girish-120@users.noreply.github.com>
2025-07-04 17:19:30 +0530

    Update city-listing.component.ts

--- src/app/city-listing/city-listing.component.ts
+++ src/app/city-listing/city-listing.component.ts
@@ -237,9 +237,9 @@ export class CityListingComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
-    this.loadZohoScript2();
-    this.getAllQuestions();
-    this.removeLoaction()
+    // this.loadZohoScript2();
+    // this.getAllQuestions();
+    // this.removeLoaction()
     this.route.params.subscribe((params: ParamMap) => {
       this.spaceType = params['spaceType'] === "coworking" ? 'coworking space' : this.getOriginalUrlParam(params['spaceType']);
       if (this.spaceType == 'coworking cafe restaurant') {
