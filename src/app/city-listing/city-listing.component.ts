dd797b07fd3cf89d1a9eadd9fc26dabea4d91863
Girish-120 <98391965+Girish-120@users.noreply.github.com>
2025-07-04 17:26:50 +0530

    Update city-listing.component.ts

--- src/app/city-listing/city-listing.component.ts
+++ src/app/city-listing/city-listing.component.ts
@@ -237,8 +237,17 @@ export class CityListingComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
-    // this.loadZohoScript2();
-    this.getAllQuestions();
+    this.loadZohoScript2();
+    // this.getAllQuestions();
+    const selectedValues = [
+      "Private Office",
+      "Managed Office",
+      "Dedicated Desk",
+      "Flexible Desk",
+      "Virtual Office",
+      "Day Pass"
+    ];
+    sessionStorage.setItem('selectedValues', JSON.stringify(selectedValues));
     this.removeLoaction()
     this.route.params.subscribe((params: ParamMap) => {
       this.spaceType = params['spaceType'] === "coworking" ? 'coworking space' : this.getOriginalUrlParam(params['spaceType']);
@@ -364,6 +373,16 @@ export class CityListingComponent implements OnInit, AfterViewInit {
         this.recommended_spaces = message
       }
     );
+    if (isPlatformBrowser(this.platformId)) {
+      if (localStorage.getItem('userDetails')) {
+        this.isCoworking = sessionStorage.getItem('isCoworking')
+        const userDetail = localStorage.getItem('userDetails');
+        const userDetailObj = JSON.parse(userDetail);
+        this.userId = userDetailObj.id
+      } else {
+        this.userId = 0
+      }
+    }
   }
 
 
